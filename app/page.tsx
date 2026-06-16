a67a026958873ebbfa8f690ab3e0c5818121075e
Silas <silasmomoh2017@gmail.com>
2026-06-16 18:27:43 +0100

    Partners

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Trust from "@/components/sections/Trust";
 import CTA from "@/components/sections/CTA";
 import Audience from "@/components/sections/Audience";
 import Resources from "@/components/sections/Resources";
-import Partners from "@/components/sections/Partners";
 import Footer from "@/components/layout/Footer";
 import PromoPopup from "@/components/ui/PromoPopup";
 import CEOInsights from "@/components/sections/CEOInsights";
@@ -22,7 +21,7 @@ export default function Home() {
       <WhyISCL />
       <Projects />
       <CEOInsights />
-      <Partners />
+  
       <CTA />
       <Footer />
     </>
